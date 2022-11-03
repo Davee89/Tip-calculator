0ac98cd080e47a45ba59cea65bbe3fc31525cb9a
Dawid Matras <davematras@gmail.com>
2022-11-03 21:44:22 +0100

    another fixes

--- script.js
+++ script.js
@@ -2,30 +2,31 @@
 // ? VARIABLES //
 const tipAmount = document.querySelector(".tip");
 const totalAmount = document.querySelector(".total");
-const bill = document.querySelector("#bill");
-const people = document.querySelector("#people");
+const billInput = document.querySelector("#bill");
+const peopleInput = document.querySelector("#people");
 const label = document.querySelector(".label");
-const btn = document.querySelectorAll(".btn");
-const custom = document.querySelector("#custom");
-const btn2 = document.querySelector(".btn2");
+const buttons = document.querySelectorAll(".btn");
+const customInput = document.querySelector("#custom");
+const buttonReset = document.querySelector(".btn2");
+const dollarSign = document.querySelector(".calculator__amount span");
 
 // * Function used below * //
-const peopleWarning = function () {
-  people.classList.add("wrong");
+const peopleWarning = () => {
+  peopleInput.classList.add("wrong");
   label.classList.remove("hidden");
 };
-const showTips = function (tip) {
-  let tipFinal = tip / Number(people.value);
-  let totalFinal = (Number(bill.value) + tip) / Number(people.value);
-  tipAmount.textContent = "$" + tipFinal.toFixed(2);
-  totalAmount.textContent = "$" + totalFinal.toFixed(2);
+const showTips = (tip) => {
+  let tipFinal = tip / peopleInput.valueAsNumber;
+  let totalFinal = billInput.valueAsNumber + tip / peopleInput.valueAsNumber;
+  tipAmount.innerText = "$" + tipFinal.toFixed(2);
+  totalAmount.innerText = "$" + totalFinal.toFixed(2);
 };
 
 // ! Calculator mechanism with normal buttons ! //
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function calculateTips() {
-    if (people.value > 0) {
-      let tip = Number(bill.value) * (parseFloat(btn[i].textContent) / 100);
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function calculateTips() {
+    if (peopleInput.value > 0) {
+      let tip = billInput.valueAsNumber * (parseFloat(buttons[i].textContent) / 100);
       showTips(tip);
     }
     // ! Not giving correct amount of people --> show warning ! //
@@ -35,9 +36,9 @@ for (let i = 0; i < btn.length; i++) {
   });
 }
 // ! Calculator mechanism with *** custom input ***  ! //
-custom.addEventListener("input", function () {
-  if (people.value > 0) {
-    let tip = Number(bill.value) * Number(custom.value / 100);
+customInput.addEventListener("input", function () {
+  if (peopleInput.value > 0) {
+    let tip = (billInput.valueAsNumber * customInput.valueAsNumber) / 100;
     showTips(tip);
   } else {
     peopleWarning();
@@ -45,19 +46,19 @@ custom.addEventListener("input", function () {
 });
 
 // * Number of people restoring styling when changed over 0 *
-people.addEventListener("input", function () {
-  if (people.value > 0) {
-    people.classList.remove("wrong");
+peopleInput.addEventListener("input", function () {
+  if (peopleInput.value > 0) {
+    peopleInput.classList.remove("wrong");
     label.classList.add("hidden");
   }
 });
 // ! RESET BUTTON ! //
-btn2.addEventListener("click", function () {
-  bill.value = "";
-  custom.value = "";
-  people.value = "";
+buttonReset.addEventListener("click", function () {
+  billInput.value = "";
+  customInput.value = "";
+  peopleInput.value = "";
   tipAmount.textContent = "$0.00";
   totalAmount.textContent = "$0.00";
-  people.classList.remove("wrong");
+  peopleInput.classList.remove("wrong");
   label.classList.add("hidden");
 });
